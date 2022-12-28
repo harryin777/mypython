9e1f9a0b5b6223fd4f2adf23cade4954df64f4be
Harryin777 <harryyin777@outlook.com>
2022-12-28 22:20:30 +0800

    update

--- mlPractice/mlPrac.py
+++ mlPractice/mlPrac.py
@@ -7,13 +7,22 @@ df = pd.read_csv("iris.data", names=cols)
 # print(df.head())
 # print(df["class"].unique())
 
+count = 0
 for item in df["class"]:
     # print(item)
     if item == 'Iris-setosa':
-        item = 1
+        df["class"][count] = 1
     elif item == 'Iris-versicolor':
-        item = 2
+        df["class"][count] = 2
     else:
-        item = 3
+        df["class"][count] = 3
+    count = count + 1
 
-print(df.head())
+for lable in cols:
+    plt.hist(df[df["class"] == 1][lable], color='blue', label='gmma', alpha=0.7, density=True)
+    plt.hist(df[df["class"] == 2][lable], color='red', label='gmma', alpha=0.7, density=True)
+    plt.title(lable)
+    plt.ylabel("Probability")
+    plt.xlabel(lable)
+    plt.legend()
+    plt.show()
