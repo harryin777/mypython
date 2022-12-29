b8c24031c959c50c53e77c559e24228e5241df46
Harryin777 <harryyin777@outlook.com>
2022-12-29 23:29:34 +0800

    update

--- mlPractice/mlPrac2.py
+++ mlPractice/mlPrac2.py
@@ -1,14 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
 df = pd.read_csv("magic04.data", names=cols)
 df.head()
 
 df["class"]=(df["class"]=="g").astype(int)
-
-print(df.head())
 
 # for label in cols:
 #     plt.hist(df[df["class"] == 1][label], color='blue', label='gmma', alpha=0.7, density=True)
@@ -20,3 +19,18 @@ print(df.head())
 #     plt.show()
 
 # train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
+
+X = df[df.columns[:-1]].values
+y = df[df.columns[-1]].values
+scaler = StandardScaler()
+print(X)
+print("-"*20)
+X = scaler.fit_transform(X)
+print(X)
+data = np.hstack((X, np.reshape(y, (-1, 1))))
+print("-"*20)
+print(data)
+print("-"*20)
+print(np.reshape(y, (-1, 2)))
+print("-"*20)
+print(y)
--- test/plotTest.py
+++ test/plotTest.py
@@ -1,9 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 # 创建一维数组
 nd1 = np.array([1, 2, 3, 3, 3, 4, 5, 6])
 # 创建二维数组
 nd2 = np.array([[1, 2, 3], [4, 5, 6]])
 
-plt.hist(nd1, density=True, align='mid')
-plt.show()
+# plt.hist(nd1, density=True, align='mid')
+# plt.show()
+
+a1 = [[[1], [2]]]
+b1 = [[[1], [2]]]
+print(np.hstack((a1, b1)))
+a = [[1], [2], [3]]
+b = [[1], [2], [3]]
+c = [[1], [2], [3]]
+d = [[1], [2], [3]]
+print(np.hstack((a, b, c, d)))
+
